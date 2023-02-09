f81f7f2b7939291b201e958bc6d7bb11a73528b4
Angelo Matteo <angmatt90@gmail.com>
2023-02-08 20:21:51 -0800

    Added a catmull rom spline generator

--- Driving Simulator (ECS)/Assets/Utils/MathLib.cs
+++ Driving Simulator (ECS)/Assets/Utils/MathLib.cs
@@ -6,6 +6,73 @@ namespace Utils
     // Much of code comes from BeamNG.drive's lua/common/mathlib.lua
     public static class MathLib
     {
+        // Source: https://en.wikipedia.org/wiki/Centripetal_Catmull%E2%80%93Rom_spline
+        // Adapted to 3D and different use case 
+        public class CatmullRomCurve
+        {
+            public Vector3 p0, p1, p2, p3;
+            public float alpha;
+
+            public CatmullRomCurve(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float alpha = 0.0f)
+            {
+                (this.p0, this.p1, this.p2, this.p3) = (p0, p1, p2, p3);
+                this.alpha = alpha;
+            }
+
+            // Evaluates a point at the given t-value from 0 to 1
+            public Vector3 GetPoint(float t)
+            {
+                // calculate knots
+                const float k0 = 0;
+                float k1 = GetKnotInterval(p0, p1);
+                float k2 = GetKnotInterval(p1, p2) + k1;
+                float k3 = GetKnotInterval(p2, p3) + k2;
+
+                // evaluate the point
+                float u = Mathf.LerpUnclamped(k1, k2, t);
+                Vector3 A1 = Remap(k0, k1, p0, p1, u);
+                Vector3 A2 = Remap(k1, k2, p1, p2, u);
+                Vector3 A3 = Remap(k2, k3, p2, p3, u);
+                Vector3 B1 = Remap(k0, k2, A1, A2, u);
+                Vector3 B2 = Remap(k1, k3, A2, A3, u);
+                return Remap(k1, k2, B1, B2, u);
+            }
+
+            static Vector3 Remap(float a, float b, Vector3 c, Vector3 d, float u)
+            {
+                return Vector3.LerpUnclamped(c, d, (u - a) / Mathf.Max(1e-30f, b - a));
+            }
+
+            float GetKnotInterval(Vector3 a, Vector3 b)
+            {
+                return Mathf.Pow((a - b).sqrMagnitude, 0.5f * alpha);
+            }
+        }
+
+        public class CatmullRomSpline
+        {
+            public Vector3[] points;
+            public float alpha;
+            public int pointCount;
+            public int segmentCount;
+
+            public CatmullRomSpline(Vector3[] points, float alpha)
+            {
+                this.points = points;
+                this.alpha = alpha;
+                this.pointCount = points.Length;
+                this.segmentCount = this.pointCount - 1;
+            }
+
+            public CatmullRomCurve GetCurve(int i)
+            {
+                Vector3 p0 = i - 1 >= 0 ? points[i - 1] : points[0];
+                Vector3 p3 = i + 2 < points.Length ? points[i + 2] : points[pointCount - 1];
+
+                return new CatmullRomCurve(p0, points[i], points[i + 1], p3, alpha);
+            }
+        }
+
         // From lua/common/filters.lua
         public class ExponentialSmoother
         {
@@ -13,12 +80,12 @@ namespace Utils
             float startingValue;
             float st;
 
-            public ExponentialSmoother(int window, float? startingValue, float? fixedDt)
+            public ExponentialSmoother(int window, float startingValue = 0.0f, float fixedDt = 0.02f)
             {
                 this.a = 2.0f / Mathf.Max(window, 2);
-                this.startingValue = startingValue ?? 0;
-                this.st = startingValue ?? 0;
-                float dt = fixedDt ?? 0.02f;
+                this.startingValue = startingValue;
+                this.st = startingValue;
+                float dt = fixedDt;
                 float adt = this.a * dt;
                 this.a = (1.0f / dt + this.a) * adt / (1 + adt);
             }
